2ae542f6d6ce881da92315cafc0f7e600be54731
Gordan Markuš <gordan.markus@pleagicore.com>
2016-10-24 11:22:17 +0200

    Check if symbolic links or files exists

--- setup-custom-zsh.sh
+++ setup-custom-zsh.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
+CUSTOM_ALIASES=$HOME/.oh-my-zsh/custom/gordan.zsh
+CUSTOM_SCREENRC=$HOME/.screenrc
+
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)" &
 wait
 
 [ ! -d $HOME/.oh-my-zsh ] && return 
 
-ln -s $PWD/custom/gordan.zsh $HOME/.oh-my-zsh/custom/gordan.zsh
-ln -s $PWD/dotfiles/.screenrc $HOME/.screenrc
+[ ! -f $CUSTOM_ALIASES ] && ln -s $PWD/custom/gordan.zsh $CUSTOM_ALIASES
+[ ! -f $CUSTOM_SCREENRC ] && ln -s $PWD/dotfiles/.screenrc $CUSTOM_SCREENRC
 
 chsh -s $(which zsh)
